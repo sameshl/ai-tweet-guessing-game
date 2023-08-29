0530a89a477842d164a4c1472f4078aef212f5f4
Samesh Lakhotia <samesh@footium.club>
2023-08-29 10:15:31 +0530

    add more sample tweets

--- src/data.ts
+++ src/data.ts
@@ -17,7 +17,7 @@ export const data: Array<SampleTweet> = [
   },
   {
     tweet: "I'm not saying that I'm an alien, but..",
-    isReal: false,
+    isReal: true,
     handle: "elonmusk",
     name: "Elon Musk",
     pfpImageLink:
@@ -40,8 +40,51 @@ export const data: Array<SampleTweet> = [
       "https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg",
   },
   {
-    tweet: "The only way you can truly lose, is if you lose to yourself.",
+    tweet:
+      "I have transcended into a viral sensation, the creator of chuckles.",
+    isReal: false,
+    handle: "elonmusk",
+    name: "Elon Musk",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg",
+  },
+  {
+    tweet:
+      "Conquering traffic congestion stands as the ultimate challenge. Even the mightiest individuals on the planet cannot overcome its grasp.",
+    isReal: false,
+    handle: "elonmusk",
+    name: "Elon Musk",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg",
+  },
+  {
+    tweet: "The one who governs the memes wields dominion over the cosmos.",
+    isReal: false,
+    handle: "elonmusk",
+    name: "Elon Musk",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg",
+  },
+  {
+    tweet: "the only way you can truly lose, is if you lose to yourself.",
     isReal: true,
+    handle: "FarzaTV",
+    name: "Farza",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1323391305834143745/4zqOJh66_400x400.jpg",
+  },
+  {
+    tweet:
+      "don't get lost in the sauce. it will often lead to low quality work and a lot of future struggle.",
+    isReal: true,
+    handle: "FarzaTV",
+    name: "Farza",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1323391305834143745/4zqOJh66_400x400.jpg",
+  },
+  {
+    tweet: `don't lose your sleep and food routine. you might end up with meh results and a bunch of headaches down the road.`,
+    isReal: false,
     handle: "FarzaTV",
     name: "Farza",
     pfpImageLink:
@@ -60,12 +103,75 @@ Don't underestimate yourself, lol. `,
       "https://pbs.twimg.com/profile_images/1323391305834143745/4zqOJh66_400x400.jpg",
   },
   {
-    tweet: `Work on your ideas on Nights and Weekends, now,
-    So that you can work on them on Mornings & Weekdays, later. `,
+    tweet: `don't lose your sleep and food routine. you might end up with meh results and a bunch of headaches down the road.`,
     isReal: false,
     handle: "FarzaTV",
     name: "Farza",
     pfpImageLink:
       "https://pbs.twimg.com/profile_images/1323391305834143745/4zqOJh66_400x400.jpg",
+  },
+  {
+    tweet: `You don’t have to wait 90-120min after waking to consume caffeine.`,
+    isReal: true,
+    handle: "hubermanlab",
+    name: "Andrew D. Huberman, Ph.D.",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1339713932085346306/jDTi4HKH_400x400.jpg",
+  },
+  {
+    tweet: `There is one science-supported approach to eliminating hiccups. And it works. `,
+    isReal: true,
+    handle: "hubermanlab",
+    name: "Andrew D. Huberman, Ph.D.",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1339713932085346306/jDTi4HKH_400x400.jpg",
+  },
+  {
+    tweet: `Stress is a call to action for growth, not a signal for defeat.`,
+    isReal: false,
+    handle: "hubermanlab",
+    name: "Andrew D. Huberman, Ph.D.",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1339713932085346306/jDTi4HKH_400x400.jpg",
+  },
+  {
+    tweet: `Remember, your brain doesn't just react to the world – it predicts and creates it.`,
+    isReal: false,
+    handle: "hubermanlab",
+    name: "Andrew D. Huberman, Ph.D.",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1339713932085346306/jDTi4HKH_400x400.jpg",
+  },
+  {
+    tweet: `Really looking forward to reviews of alien technology`,
+    isReal: true,
+    handle: "MKBHD",
+    name: "Marques Brownlee",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1468001914302390278/B_Xv_8gu_400x400.jpg",
+  },
+  {
+    tweet: `Nooooooooooooooooo`,
+    isReal: true,
+    handle: "MKBHD",
+    name: "Marques Brownlee",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1468001914302390278/B_Xv_8gu_400x400.jpg",
+  },
+  {
+    tweet: `Tech excellence isn't just about specs; it's about the seamless marriage of hardware and experience.`,
+    isReal: false,
+    handle: "MKBHD",
+    name: "Marques Brownlee",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1468001914302390278/B_Xv_8gu_400x400.jpg",
+  },
+  {
+    tweet: `Simple design: less head-scratching, more using.`,
+    isReal: false,
+    handle: "MKBHD",
+    name: "Marques Brownlee",
+    pfpImageLink:
+      "https://pbs.twimg.com/profile_images/1468001914302390278/B_Xv_8gu_400x400.jpg",
   },
 ];
